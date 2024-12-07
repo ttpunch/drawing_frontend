409e8b3228025f7478a778298d5a34189f57cc29
ttpunch <vinod418@gmail.com>
2024-12-07 22:20:30 +0530

    all woking

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useAuthStore } from '../store/authStore';
 
 const api = axios.create({
-  baseURL: 'https://drawing-backend-15rx.onrender.com/api',
+  baseURL: 'http://localhost:3000/api',
   withCredentials: true,
 });
 
